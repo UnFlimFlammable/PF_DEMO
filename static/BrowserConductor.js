d644e506aa5c798601d9e821549f7b9ba9771972
caylebray <braycayle@gmail.com>
2015-10-23 10:21:41 -0600

    Fixed app so clicking on an account summary only lists transactions for that account

--- static/BrowserConductor.js
+++ static/BrowserConductor.js
@@ -36,7 +36,8 @@ $(document).ready(function(){
 
   $(document).on('click', '.account', function(event){
     var row = this;
-    console.log(this)
+    console.log($(row).attr("AccountJSON"));
+    var account = JSON.parse($(row).attr("AccountJSON"));
     $.ajax("htmlFragment?file=accountSummary.html",{
       success: function(data, textStatus, jqXHR){
         $("#accountSummary").empty();
@@ -46,11 +47,11 @@ $(document).ready(function(){
 
         $("#accountSummaryBalance").text();
         var table = $("#transactionTable");
-        for(var i = 0; i < currentUser.accounts.length; i++){
-          for(var c = 0; c < currentUser.accounts[i].transactions.length; c++){
+
+          for(var c = 0; c < account.transactions.length; c++){
             $("#transactionTable").append("<tr></tr>");
             var row = $("#transactionTable tr:last");
-            var transaction = currentUser.accounts[i].transactions[c];
+            var transaction = account.transactions[c];
 
             row.append("<td>" + transaction.kind + "</td>");
             row.append("<td>" + transaction.recipient + "</td>");
@@ -60,7 +61,6 @@ $(document).ready(function(){
 
             //TODO Find some way to pass an account into this, look into sending it back in the header
           }
-        }
       }
     });
   });
